8946880c325d9cf8d494f3c280f2017381e964e8
baranov34@gmail.com <ursus1ursus1>
2018-05-28 19:02:42 +0300

    Revert "Revert "Formally finished, but needs another rewiew and codestyle improvement.""
    This reverts commit 8a9489fdb8965c722cd18bd59ca41463ee5a1e79.

--- Src/keyboard.c
+++ Src/keyboard.c
@@ -1,4 +1,5 @@
 #include "keyboard.h"
+#include "cmsis_os.h"
 
 const char kb_symbols[KEYBOARD_ROWS][KEYBOARD_COLUMNS] = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' }, { '*', '0', '#' } };
 
@@ -19,17 +20,19 @@ void KeyboardInit(void) {
 
 void KeyboardPoll(char* kb_out) {
 	char c;
-	char *pChar = &c;
 	uint8_t num_keys_pressed = 0;
-	kb_out[0] = '\0';
+	memset(kb_out, 0, KEYBOARD_MAX_MULTITOUCH);
 	for (uint8_t col = 0; col < KEYBOARD_COLUMNS; col++) {
 		HAL_GPIO_WritePin(CLM_GPIO_Port, (uint16_t) clm_pins[col], SET);
+		osDelay(1);
 		for (uint8_t row = 0; row < KEYBOARD_ROWS; row++) {
 			if (GPIO_PIN_SET == HAL_GPIO_ReadPin(ROW_GPIO_Port, (uint16_t) row_pins[row])) {
-				num_keys_pressed++;
+
 				c = (char) kb_symbols[row][col];
-				strcat(kb_out, pChar);
+				kb_out[num_keys_pressed] = c;
+				num_keys_pressed++;
 				if (num_keys_pressed > KEYBOARD_MAX_MULTITOUCH) {
+					memset(kb_out, 0, KEYBOARD_MAX_MULTITOUCH);
 					kb_out[0] = '\0';
 					return;
 				}
